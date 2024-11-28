35f0998c86c38d87d6596b7c64231d325841e5c0
RandomStuff45 <luca.s.salazar@hotmail.com>
2024-11-28 18:15:35 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -9,6 +9,12 @@ import time
 
 money = 100000
 
+def gamepart2n():
+    playagain()
+
+def gamepart2s():
+    playagain()
+
 def playagain():
     """Asks to play again if the player already played"""
     play_again = input("Do you want to play again? Y/N\n")
@@ -56,24 +62,21 @@ def start():
         print("That's not an answer. Only type 'Y' or 'N'.")
         start()
 
-
 def gamepart1():
     """The actual game part 1"""
     global money
     amount_betting_input = input("How much are you betting?\n")
     amount_betting = int(re.search(r'\d+', amount_betting_input).group())
-    playing = True
-    while playing is True:
-        player_card_count1 = rnd.randint(1, 13)
-        player_card_count2 = rnd.randint(1, 13)
-        current_count = player_card_count1 + player_card_count2
-        if current_count > 21:
-            print("You already busted. Hope you did not bet too much!")
-            current_money = money - amount_betting
-            money = current_money
-            playagain()
-        else:
-            input(f"Your current card value is {current_count}. Press any key to continue.")
+    player_card_count1 = rnd.randint(1, 13)
+    player_card_count2 = rnd.randint(1, 13)
+    current_count = player_card_count1 + player_card_count2
+    if current_count > 21:
+        print("You already busted. Hope you did not bet too much!")
+        current_money = money - amount_betting
+        money = current_money
+        playagain()
+    else:
+        input(f"Your current card value is {current_count}. Press any key to continue.")
     if player_card_count1 == player_card_count2:
         gamepart2s()
     else:
